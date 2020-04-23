984e7705cd1a27861cb65db3473e2e4598ffa253
uberto <uberto.gama@gmail.com>
2020-04-23 23:34:01 +0100

    better explanations

--- scripts/releaseOnMavenCentral.sh
+++ scripts/releaseOnMavenCentral.sh
@@ -1,4 +1,9 @@
 ./gradlew uploadArchives -p pesticide-core
 
 
-#ftm you need to update the version manually and for non snapshop to close the repository from sonatype site
+# before launching it you need to update the version manually (remove snapshot)
+# and then bouncing the version with SNAPSHOT after
+# then go to sonatype site and login
+# https://oss.sonatype.org/#nexus-search;quick~pesticide
+# select Staging Repositories, and close the corresponding one.
+# then click release and wait 10 min
