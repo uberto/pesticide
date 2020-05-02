6cd0b0fefe17e0c2588c68ce3d988b7094670455
uberto <uberto.gama@gmail.com>
2020-05-02 09:59:17 +0100

    0.9.9 SNAPSHOT

--- scripts/releaseOnMavenCentral.sh
+++ scripts/releaseOnMavenCentral.sh
@@ -1,9 +1,13 @@
+
+# Steps:
+# remove snapshot from the version manually in build.gradle
+# update the README.md
+# launch:
+
 ./gradlew uploadArchives -p pesticide-core
 
-
-# before launching it you need to update the version manually (remove snapshot)
-# and then bouncing the version with SNAPSHOT after
 # then go to sonatype site and login
 # https://oss.sonatype.org/#nexus-search;quick~pesticide
 # select Staging Repositories, and close the corresponding one.
 # then click release and wait 10 min
+# and then bouncing the version with SNAPSHOT in build.gradle
